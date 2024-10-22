e690548c8209b8be988754774550470f4afbb77e
jjosegallegocesde <jjosegallegomesa@gmail.com>
2024-10-22 11:46:47 -0500

    ok se crean formualrios

---
+++ src/controllers/controladorRegistroZona.js
@@ -0,0 +1,30 @@
+let botonZona=document.getElementById("botonZona")
+
+let cajaNombreZona=document.getElementById("nombreZona")
+let cajaVolumenZona=document.getElementById("volumenZona")
+let cajaPesoZona=document.getElementById("pesoZona")
+
+let formualrioZona=document.getElementById("formualriozona")
+
+botonZona.addEventListener("click",function(evento){
+    
+    let datosZona={
+        nombreZona:cajaNombreZona.value,
+        volumenMaximo:cajaPesoZona.value,
+        pesoMaximo:cajaPesoZona.value
+
+    }
+
+    formualrioZona.reset()
+
+    Swal.fire({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success"
+    });
+    console.log(datosZona)
+    
+    //llamar al servicio para entregarle los datos
+
+
+})
